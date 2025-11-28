a11f594e2496f1b11c9321058e45885d019053a1
artinus132-coding <artinus132@guru.smp.belajar.id>
2025-11-28 10:37:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+//Pratinjau Gambar
+document.getElementById('imageInput').addEventListener('change', function(event){
+const file = event.target.files[0];
+if (file){
+  const imgPreview = document.getElementById('imgPreview');
+  imgPreview.src = URL.createObjectURL(file);
+  imgPreview.style.display = 'block';
+}
+});
 
+//Pratinjau Video
+document.getElementById('videoInput').addEventListener('change', function(event){
+const file = event.target.files[0];
+ if (file){
+  const videoPreview = document.getElementById('videoPreview');
+  videoPreview.src = URL.createObjectURL(file);
+  videoPreview.style.display = 'block'
+ }
+});
